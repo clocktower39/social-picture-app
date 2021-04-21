19df90444d87c1f983665ba2d6f43963b32690bb
clocktower39 <clocktower39@aol.com>
2021-04-20 19:06:31 -0700

    search for users

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect }  from 'react';
 import { connect } from 'react-redux'
-import { CardMedia, Container, Grid, TextField, makeStyles } from '@material-ui/core';
+import { Button, Typography, Container, Grid, TextField, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
     root: {
@@ -10,6 +10,8 @@ const useStyles = makeStyles({
 export const Search = (props) => {
     const classes = useStyles();
     const [opacity, setOpacity] = useState(0);
+    const [searchInput, setSearchInput] = useState('');
+    const [users, setUsers] = useState([]);
 
     useEffect(()=>{
         if(opacity<1){
@@ -17,17 +19,45 @@ export const Search = (props) => {
         }
         // eslint-disable-next-line
     },[opacity]);
+
+    const fetchSearch = () => {
+        let payload = JSON.stringify({ username: searchInput});
+
+        fetch('http://mattkearns.ddns.net:3000/search', {
+            method: 'post',
+            dataType: 'json',
+            body: payload,
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            }
+          })
+          .then(res => res.json())
+          .then(data => {
+              setUsers(data.users);
+          });
+    }
+
+    const handleInput = (e) => {
+        setSearchInput(e.target.value);
+    }
     
     return (
         <Container disableGutters maxWidth='sm' className={classes.root} style={{opacity}}>
             <Grid container>
-                <Grid item xs={12}>
-                    <TextField fullWidth label='Search' />
+                <Grid item xs={10}>
+                    <TextField fullWidth label='Search' onChange={handleInput} value={searchInput} />
+                </Grid>
+                <Grid item xs={2}>
+                    <Button variant='contained' color='primary' fullWidth onClick={fetchSearch} >Search</Button>
                 </Grid>
                 {/* pull X random users then a random post from them */}
-                        <Grid item xs={4}>
-                            <CardMedia className={classes.media} image={'#'}/>
-                        </Grid>
+                {users.map((user, index) => {
+                    return(
+                    <Grid item xs={12}>
+                        <Typography>{user.username}</Typography>
+                    </Grid>
+                    )
+                })}
             </Grid>
         </Container>
     )
