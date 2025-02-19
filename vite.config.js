916f94a1e99cd6eba1a6a3fa0294fd6b7f9571c4
clocktower39 <clocktower39@aol.com>
2025-02-19 00:05:37 -0700

    configure chunking, remove unused packages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "mui-vendor": [
+            "@mui/material",
+            "@mui/icons-material",
+            "@mui/styles",
+            "@mui/system",
+            "@mui/x-date-pickers",
+            "@emotion/react",
+            "@emotion/styled",
+          ],
+          "redux-vendor": ["react-redux", "redux", "redux-thunk", "@redux-devtools/extension"],
+          utils: ["axios", "jwt-decode", "socket.io-client"],
+        },
+      },
+    },
+  },
+});
